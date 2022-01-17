551c8ab0331c1e45b036c6d18459ac203e39538d
Noman Ali <33200799+nomi181472@users.noreply.github.com>
2022-01-17 21:37:52 +0500

    utilizing Geolocation flutter package

---
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,33 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initstate() {
+    super.initState();
+    // getGeoLocation();
+  }
+
+  void getGeoLocation() async {
+    Location location = new Location();
+    await location.getCurrentLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {},
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
